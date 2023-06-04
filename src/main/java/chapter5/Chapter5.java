99b9f3fcb37780801844ac9f29d0db11e6ac4c31
goldendk <goldendk@hotmail.com>
2023-06-04 15:15:51 +0200

    Adding chapter 5

---
+++ src/main/java/chapter5/Chapter5.java
@@ -0,0 +1,56 @@
+package chapter5;
+
+import java.util.Arrays;
+
+public class Chapter5 {
+
+    public static void main(String... args){
+
+
+        System.out.println("foo");
+
+
+        System.out.println("");
+        System.out.println("args = " + args);
+
+        System.out.println("Chapter5.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+
+        String name;
+        String names[] = {"foo", "bar"};
+
+
+        int length = names.length;
+        var length1 = names.length;
+
+        int length2 = names.length;
+
+
+        int length3 = names.length;
+
+
+        int length4 = names.length;
+
+
+        int length5 = names.length;
+
+        int length6 = names.length;
+
+        int length7 = names.length;
+
+        int length8 = names.length;
+
+        int length9 = names.length;
+
+
+        int length10 = names.length;
+
+
+        int length11 = names.length;
+
+
+
+
+
+    }
+}
