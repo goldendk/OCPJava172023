ba4595e61a8ee7dac6bbd27776ab24fd070e6fcc
goldendk <goldendk@hotmail.com>
2023-06-18 21:57:00 +0200

    just updating code.

---
+++ src/main/java/package12/GenericsMain.java
@@ -0,0 +1,26 @@
+package package12;
+
+import java.util.List;
+
+public class GenericsMain {
+
+    public static void print(List<? extends Number> list) {
+        for (Number n : list)
+            System.out.print(n + " ");
+        System.out.println();
+    }
+
+    public class Singleton<T> {
+
+        public static <T> T getInstance() {
+            // if (instance == null);
+            //  instance = new Singleton<T>();
+
+            //      return (T) instance;
+            return null;
+        }
+
+        //   private static T instance = null; //not allowed.
+
+    }
+}
