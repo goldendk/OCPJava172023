d424effd1abd5e7939c1ef71570f167be332cf1f
goldendk <goldendk@hotmail.com>
2023-09-03 21:52:46 +0200

    just updating code.

--- src/main/java/book/overrideoverload/SubClass.java
+++ src/main/java/book/overrideoverload/SubClass.java
@@ -1,4 +1,22 @@
 package book.overrideoverload;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class SubClass extends SuperClass {
+    //overrides correctly - the only one who does.
+    public <T> List<T> getCollection(T t, Integer m){ return new ArrayList<T>(); }; // 1
+     //public <T> Stream<T> getCollection(T t, Integer m){ return new ArrayList<T>(); }; //2
+
+    //non co-variant return type.
+    //public <T> void getCollection(T t, Integer m){ return new ArrayList<T>(); }; //3
+
+
+    //just a method. does not override or overload.
+     public <T> List<T> getCollection(String t, Integer m){ return new ArrayList<T>(); };//4
+
+    //overloads //4
+     public <T> Object getCollection(String t, Integer m, Float f){ return new ArrayList<T>(); };//4a
 }
--- src/main/java/book/overrideoverload/SuperClass.java
+++ src/main/java/book/overrideoverload/SuperClass.java
@@ -1,4 +1,10 @@
 package book.overrideoverload;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class SuperClass {
+    public <T> Collection<T> getCollection(T t, Integer n) {
+        return new ArrayList<T>();
+    }
 }
